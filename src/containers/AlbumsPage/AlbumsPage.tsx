83ced050ee6eaaff6c073da96b3e3cf00837c414
Mihir Jayavant <Mihir.Jayavant2@gmail.com>
2020-05-09 13:00:35 +0530

    modal added

--- src/containers/AlbumsPage/AlbumsPage.tsx
+++ src/containers/AlbumsPage/AlbumsPage.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const AlbumsPage = () => {
-  return <div className="nav-margin">Albums</div>
+function AlbumsPage() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => setIsOpen(!isOpen)
+
+  return (
+    <div className="AlbumPage">
+      <button className="button is-primary" onClick={toggle}>
+        Open
+      </button>
+
+      <div className={isOpen ? 'modal is-active' : 'modal'}>
+        <div className="modal-background" />
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">Modal title</p>
+            <button className="delete" aria-label="close" onClick={toggle} />
+          </header>
+          <section className="modal-card-body" />
+          <footer className="modal-card-foot">
+            <button className="button is-success">Save changes</button>
+            <button className="button">Cancel</button>
+          </footer>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default AlbumsPage
